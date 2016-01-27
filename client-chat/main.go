3e0a67a1c97f8eade46cb585dcb6d6af8d1060ab
chikilov <cm@anticgame.co.kr>
2016-01-27 18:29:28 +0900

    just simple chatting server

---
+++ client-chat/main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"bufio"
+	"common"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+)
+
+const (
+	CONN_HOST = "localhost"
+	CONN_PORT = "3333"
+	CONN_TYPE = "tcp"
+)
+
+func main() {
+	tcpAddr, err := net.ResolveTCPAddr("tcp", CONN_HOST+":"+CONN_PORT)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Connect to server through tcp.
+	conn, err := net.DialTCP("tcp", nil, tcpAddr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+	go printOutput(conn)
+	writeInput(conn)
+}
+
+func writeInput(conn *net.TCPConn) {
+	fmt.Print("Enter username: ")
+	// Read from stdin.
+	reader := bufio.NewReader(os.Stdin)
+	username, err := reader.ReadString('\n')
+	username = username[:len(username)-1]
+	if err != nil {
+		log.Fatal(err)
+	}
+	str, err := json.Marshal(map[string]interface{}{"username": string(username)})
+	if err != nil {
+		fmt.Println("It is not property name")
+		return
+	}
+	err = common.WriteMsg(conn, string(str))
+	if err != nil {
+		log.Println(err)
+	}
+	fmt.Println("Enter text: ")
+	for {
+		text, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+		err = common.WriteMsg(conn, username+": "+text)
+		if err != nil {
+			log.Println(err)
+		}
+	}
+}
+
+func printOutput(conn *net.TCPConn) {
+	for {
+
+		msg, err := common.ReadMsg(conn)
+		// Receiving EOF means that the connection has been closed
+		if err == io.EOF {
+			// Close conn and exit
+			conn.Close()
+			fmt.Println("Connection Closed. Bye bye.")
+			os.Exit(0)
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(msg)
+	}
+}
---
+++ server-chat/main.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"common"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+)
+
+const (
+	CONN_HOST = "0.0.0.0"
+	CONN_PORT = "3333"
+	CONN_TYPE = "tcp"
+)
+
+type netCon struct {
+	connection     net.Conn
+	username       string
+	isHandShakeing bool
+}
+
+var (
+	netCons []netCon
+)
+
+func main() {
+	// Listen for incoming connections.
+	l, err := net.Listen(CONN_TYPE, CONN_HOST+":"+CONN_PORT)
+	if err != nil {
+		fmt.Println("Error listening:", err.Error())
+		os.Exit(1)
+	}
+	// Close the listener when the application closes.
+	defer l.Close()
+	fmt.Println("Listening on " + CONN_HOST + ":" + CONN_PORT)
+	for {
+		// Listen for an incoming connection.
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting: ", err.Error())
+			os.Exit(1)
+		}
+		var newCon []netCon
+		var tmpCon netCon
+		tmpCon.connection = conn
+		tmpCon.isHandShakeing = false
+		newCon = append(newCon, tmpCon)
+		// Save connection
+		netCons = append(netCons, newCon...)
+		fmt.Printf("--------\n")
+		// Handle connections in a new goroutine.
+		go handleRequest(newCon[0])
+	}
+}
+
+// Handles incoming requests.
+func handleRequest(newCon netCon) {
+	for {
+		msg, err := common.ReadMsg(newCon.connection)
+		if err != nil {
+			if err == io.EOF {
+				// Close the connection when you're done with it.
+				removeConn(newCon.connection)
+				newCon.connection.Close()
+				return
+			}
+			log.Println(err)
+			return
+		}
+		if newCon.isHandShakeing {
+			fmt.Printf("Message Received: %s\n", msg)
+			fmt.Printf("%t\n", newCon.isHandShakeing)
+			for i := range netCons {
+				fmt.Printf("%s : %t\n", netCons[i].username, netCons[i].isHandShakeing)
+			}
+			broadcast(newCon, msg)
+		} else {
+			var y map[string]interface{}
+			json.Unmarshal([]byte(msg), &y)
+			for n, v := range y {
+				if n == "username" {
+					for i := range netCons {
+						if netCons[i] == newCon {
+							netCons[i].isHandShakeing = true
+							netCons[i].username = v.(string)
+							newCon = netCons[i]
+						}
+					}
+				}
+			}
+			fmt.Printf("Handshake success: %s\n", newCon.username)
+			broadcast(newCon, newCon.username+" has join now")
+		}
+	}
+}
+
+func removeConn(conn net.Conn) {
+	var i int
+	for i = range netCons {
+		if netCons[i].connection == conn {
+			break
+		}
+	}
+	fmt.Println(i)
+	netCons = append(netCons[:i], netCons[i+1:]...)
+}
+
+func broadcast(newCon netCon, msg string) {
+	for i := range netCons {
+		if netCons[i].isHandShakeing {
+			err := common.WriteMsg(netCons[i].connection, msg)
+			if err != nil {
+				log.Println(err)
+			}
+		}
+	}
+}
